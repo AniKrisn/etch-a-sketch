5bb9aa974a165e970e34071b8a89eea757dacf2b
akrishnan <anikrishnantango@gmail.com>
2022-09-01 11:33:01 +0100

    add container and initial box to DOM

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+const box = document.createElement('div');
+
+box.classList.add('box');
+container.appendChild(box);
+
+
+for (i=0, i<=16; i++) {
+    const smallBox = document.createElement('div');
+    smallBox.classList.add('smallbox')
+    
+}
+
+
+
+
+
+
